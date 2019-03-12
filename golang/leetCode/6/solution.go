b3dbe635219e8b803d3805d974d39709870fd2d3
huirenjie <hui.renjie@whaley.cn>
2019-03-12 15:51:51 +0800

    add leetCode 4/5/6

---
+++ golang/leetCode/6/solution.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+)
+
+/*
+第一次时的思路：
+
+*/
+
+func convert(s string, numRows int) string {
+
+}
+
+func main() {
+	s := "babaaaaaadada"
+	numRows := 3
+	result := convert(s, numRows)
+	fmt.Println(result)
+}
