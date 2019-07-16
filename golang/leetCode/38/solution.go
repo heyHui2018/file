30f1b2b91dbc3c4bf1855bf16e35730d6a3d3217
huirenjie <hui.renjie@whaley.cn>
2019-07-16 17:34:20 +0800

    add leetCode 35/36/37

---
+++ golang/leetCode/38/solution.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+)
+
+/*
+要求:
+1:1
+2:1个1->11
+3:2个1->21
+4:1个2、1个1->1211
+5:1个1、1个2、2个1->111221
+6:3个1、2个2、1个1->312211
+
+
+尝试：
+
+
+学习：
+
+
+关键点：
+
+*/
+
+func countAndSay(n int) string { // faster 66.34% less 82.86%
+
+}
+
+func main() {
+	result := countAndSay(4)
+	fmt.Println(result)
+}
