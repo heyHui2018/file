79b49c5a944304df3201ce73c0602b023b92149f
huirenjie <hui.renjie@whaley.cn>
2019-07-22 13:26:24 +0800

    add leetCode 39/40/41

---
+++ golang/leetCode/42/solution.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+)
+
+/*
+要求:
+
+尝试：
+
+学习：
+
+关键点：
+
+
+*/
+
+func trap(height []int) int { // faster 100% less 98.53%
+
+}
+
+func main() {
+	height := []int{0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1}
+	result := trap(height)
+	fmt.Println(result)
+}
