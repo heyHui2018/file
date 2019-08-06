306a3c014e5d63374fbb5f961146a8d6cfadee2c
heyHui2018 <962691478@qq.com>
2019-08-06 19:10:22 +0800

    add leetCode 52

---
+++ golang/leetCode/53.Maximum Subarray/solution.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+)
+
+/*
+要求：
+
+
+解题思路：
+
+
+关键点：
+
+
+*/
+
+func maxSubArray(nums []int) int { // faster 100% less 50%
+
+}
+
+func main() {
+	nums := []int{-2, 1, -3, 4, -1, 2, 1, -5, 4}
+	result := maxSubArray(nums)
+	fmt.Println(result)
+}
