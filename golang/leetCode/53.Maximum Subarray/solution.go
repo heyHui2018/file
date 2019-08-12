b0734b6a23c691b7bca46c8ff2f1d25eb298647e
heyHui2018 <962691478@qq.com>
2019-08-12 15:42:02 +0800

    add bubbleSort/insertionSort/mergeSort/selectionSort/shellSort/leetCode 53/54/55

--- golang/leetCode/53.Maximum Subarray/solution.go
+++ golang/leetCode/53.Maximum Subarray/solution.go
@@ -9,15 +9,27 @@ import (
 
 
 解题思路：
-
+遍历nums,n对应的sum为n与n-1对应的sum中的较大值,同时更新maxSum
 
 关键点：
-
+当前index能组成的最大sum,是以index-1能组成的最大sum为基础的
 
 */
 
-func maxSubArray(nums []int) int { // faster 100% less 50%
+func maxSubArray(nums []int) int { // faster 97.18% less 59.26%
+	sum, maxSum := -1<<31, -1<<31
+	for _, n := range nums {
+		sum = max(sum+n, n)
+		maxSum = max(maxSum, sum)
+	}
+	return maxSum
+}
 
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
 }
 
 func main() {
---
+++ golang/leetCode/56.Merge Intervals/solution.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+)
+
+/*
+要求：
+
+
+解题思路：
+
+
+关键点：
+
+
+*/
+
+func merge(intervals [][]int) [][]int { // faster 95.40% less 85.71%
+
+}
+
+func main() {
+	nums := [][]int{{2}, {0}, {0}}
+	result := merge(nums)
+	fmt.Println(result)
+}
