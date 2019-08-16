e3301050b863f42c4321e2ab9384c8bffe92d31a
heyHui2018 <962691478@qq.com>
2019-08-16 15:27:35 +0800

    add leetCode 59

---
+++ golang/leetCode/60.Permutation Sequence/solution.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+)
+
+/*
+要求：
+
+
+解题思路：
+
+
+关键点：
+
+
+*/
+
+func getPermutation(n int, k int) string { // faster 100% less 100%.
+
+}
+
+func main() {
+	n := 3
+	k := 6
+	result := getPermutation(n, k)
+	fmt.Println(result)
+}
